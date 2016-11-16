8223dab73c50b89bce43f189752b3f94ef1db386
Chris Hatton <christopherhattonuk@gmail.com>
2016-11-16 18:53:25 +1100

    Updated comments

--- AVRInsults/AVRInsults.c
+++ AVRInsults/AVRInsults.c
@@ -24,25 +24,28 @@ int main (void)
 	stdout = &uart_output;
 	stdin  = &uart_input;
 
-	UCSR0B = (1 << RXEN0)  | (1 << TXEN0);   // Turn on the transmission and reception circuitry
+	UCSR0B = (1 << RXEN0)  | (1 << TXEN0);  // Turn on the transmission and reception circuitry
 	UCSR0C = (1 << UCSZ00) | (1 << UCSZ01); // Use 8-bit character sizes
 
-	UBRR0H = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
-	UBRR0L = BAUD_PRESCALE; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
+	UBRR0H = (BAUD_PRESCALE >> 8);
+	UBRR0L =  BAUD_PRESCALE;
 
-	printf("You idiot");
+	while(1)
+	{
+		printf("You idiot\n"); // Starting simple: the first insult, issued repeatedly.
+	}
 }
 
 void uart_putchar(char c, FILE *stream)
 {
-	while ((UCSR0A & (1 << UDRE0)) == 0) {}; // Do nothing until UDR is ready for more data to be written to it
-	UDR0 = c; // Echo back the received byte back to the computer
+	while ((UCSR0A & (1 << UDRE0)) == 0) {};
+	UDR0 = c;
 }
 
 char uart_getchar(FILE *stream)
 {
-	while ((UCSR0A & (1 << RXC0)) == 0) {}; // Do nothing until data have been received and is ready to be read from UDR
-	return UDR0; // Fetch the received byte value into the variable "ByteReceived"
+	while ((UCSR0A & (1 << RXC0)) == 0) {}; 
+	return UDR0;
 }
 
 void setLED(int on)
