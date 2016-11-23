a55aac92a2668e0f4fb781dbdc7e2b60075ddd9b
Chris Hatton <christopherhattonuk@gmail.com>
2016-11-23 21:54:08 +1100

    Added Talkie Project dependency to AVRStudio Solution Added some preliminary string wrangling code (to be re-worked)

--- AVRInsults/AVRInsults.c
+++ AVRInsults/AVRInsults.c
@@ -3,6 +3,7 @@
 #include "InsultData.h"
 #include <stdio.h>
 #include <avr/pgmspace.h>
+#include <talkie.h>
 
 void readToken(char target[], int index);
 
@@ -10,13 +11,56 @@ int main (void)
 {
 	initStdInOutUart();
 
+	char** strings;
+	int stringIndex = 0;
+	strings[0] = "%root%";
+	const char tokenDelimeter='%';
+	int searchOffset = 0;
+
 	while(1)
 	{	
 		// Retrieve a token string from program space and print it
+		/*
+
+		char c = strings[stringIndex][searchOffset];
+		do 
+		{
+			if(c==tokenDelimeter)
+			{
+				int tokenStartOffset = searchOffset;
+				
+				do // Get token name
+				{
+					++searchOffset;
+					c = strings[stringIndex][searchOffset];
+				}
+				while(c!=tokenDelimeter)
+
+				int tokenEndOffset = searchOffset;
+
+				int tokenLength = tokenEndOffset - tokenStartOffset;
+				
+				char *token = str
+
+				// Get token replacement
+				char *tokenReplacement = copyReplacementForToken(  )
+				
+
+				// Realloc next string to accomodate token replacement
+				
+				// Copy in token replacement
+				
+				
+			}
+			
+			++searchOffset;
+		}
+		while ( c != 0x00 ); // Reached end of string without token replacement
 		
 		char aToken[100];
 		readToken(aToken,2);
 		printf("%s\n", aToken);
+		*/
 	}
 }
 
